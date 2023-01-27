c88e8ba4270357b128197c5ec298d63011284198
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-01-27 18:36:01 +0600

    router added

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
 import Layout from './Layout';
-// import Home from './pages/Home';
-// import Signup from './pages/Signup';
-// import Quiz from './pages/Quiz';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Quiz from './pages/Quiz';
 import Result from './pages/Result';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Quiz /> */}
-      <Result />
-    </Layout>
+    // before routing
+    // <Layout>
+    //   <Home />
+    //   <Signup />
+    //   <Login />
+    //   <Quiz />
+    //   <Result />
+    // </Layout>
+
+    <Router>
+      <Layout>
+      {/* instead of "Switch" */}
+        <Routes> 
+          {/* <Route exact path="/" component={Home}/> */}
+          <Route path='/' element={<Home />}/>
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/quiz' element={<Quiz />}/>
+          <Route path='/result' element={<Result />}/>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
