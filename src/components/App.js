72731e81dbaf9c25f0d2965f02854ecfcf37d19b
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-01-28 11:53:58 +0600

    added private route using Outlet

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,27 @@ import Login from './pages/Login';
 import Quiz from './pages/Quiz';
 import Result from './pages/Result';
 import Signup from './pages/Signup';
-
+import PrivateOutlet from './PrivateOutlet';
+import PublicOutlet from './PublicOutlet';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Layout>
-        {/* instead of "Switch" */}
           <Routes> 
-            {/* <Route exact path="/" component={Home}/> */}
             <Route path='/' element={<Home />}/>
-            <Route path='/signup' element={<Signup />}/>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/quiz' element={<Quiz />}/>
-            <Route path='/result' element={<Result />}/>
+
+            <Route path='/' element={<PublicOutlet />}>
+              <Route path='/signup' element={<Signup />}/>
+              <Route path='/login' element={<Login />}/>
+            </Route>
+            
+            <Route path='/' element={<PrivateOutlet />}>
+              <Route path='/quiz' element={<Quiz />}/>
+              <Route path='/result' element={<Result />}/>
+            </Route>
+
           </Routes>
         </Layout>
       </AuthProvider>
