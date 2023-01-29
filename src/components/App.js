0b263ee0e8359dff98d20644ad3c06d96c65dc2c
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-01-29 21:46:37 +0600

    firebase database fetch

--- src/components/App.js
+++ src/components/App.js
@@ -24,8 +24,8 @@ function App() {
             </Route>
             
             <Route path='/' element={<PrivateOutlet />}>
-              <Route path='/quiz' element={<Quiz />}/>
-              <Route path='/result' element={<Result />}/>
+              <Route path='/quiz/:id' element={<Quiz />}/>
+              <Route path='/result/:id' element={<Result />}/>
             </Route>
 
           </Routes>
